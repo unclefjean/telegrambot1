b4ea5246036859491193768aeb1a25cea6145069
unclefjean <83767186+unclefjean@users.noreply.github.com>
2024-11-30 17:45:32 +0500

    Update background.py

--- background.py
+++ background.py
@@ -1,40 +1,30 @@
 from flask import Flask
-from flask import request
 from threading import Thread
 import time
 import requests
+import os
 
 app = Flask('')
 
-
 @app.route('/')
 def home():
-  return "I'm alive"
-
+    return "I'm alive!"
 
 def run():
-  app.run(host='0.0.0.0', port=80)
-
-
-def keep_alive():
-  t = Thread(target=run)
-  t.start()
-
+    port = os.environ.get('PORT', 80)  # Используйте PORT из окружения или 80 по умолчанию
+    app.run(host='0.0.0.0', port=port)
 
 def ping_self():
-  while True:
-    try:
-      requests.get(
-          "https://22bea987-f52f-4ef8-9020-6b4089dcb8cc-00-isplf5f9wlnr.pike.replit.dev/"
-      )
-      print("Self-ping successful")
-    except Exception as e:
-      print(f"Self-ping failed: {e}")
-    time.sleep(240)  # Каждые 4 минуты
+    while True:
+        try:
+            requests.get("https://<ваша ссылка>.replit.dev/")
+            print("Self-ping successful")
+        except Exception as e:
+            print(f"Self-ping failed: {e}")
+        time.sleep(240)
 
-
-if __name__ == "__main__":
-  t1 = Thread(target=run)
-  t2 = Thread(target=ping_self)
-  t1.start()
-  t2.start()
+def keep_alive():
+    t1 = Thread(target=run)
+    t2 = Thread(target=ping_self)
+    t1.start()
+    t2.start()
